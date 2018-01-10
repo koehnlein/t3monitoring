fc679168c103255a4eb285b845a8968503fdc5d9
Georg Ringer <georg.ringer@gmail.com>
2018-01-10 23:23:24 +0100

    [TASK] Allow 9.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,15 +3,15 @@
 $EM_CONF[$_EXTKEY] = [
     'title' => 'Monitoring for TYPO3 installations',
     'description' => '',
-    'category' => 'plugin',
+    'category' => 'be',
     'author' => 'Georg Ringer',
     'author_email' => '',
     'state' => 'beta',
-    'clearCacheOnLoad' => 0,
+    'clearCacheOnLoad' => 1,
     'version' => '2.0.0',
     'constraints' => [
         'depends' => [
-            'typo3' => '8.7.6-8.99.99',
+            'typo3' => '8.7.6-9.0.99',
         ],
         'conflicts' => [],
         'suggests' => [],
